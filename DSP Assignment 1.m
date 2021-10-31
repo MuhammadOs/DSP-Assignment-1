4665688bd526d5ec2460add83360cbf001ec8cec
Muhammad Osama <82714930+MuhammadOs@users.noreply.github.com>
2021-10-31 22:07:49 +0200

    Add files via upload

---
+++ DSP Assignment 1.m
@@ -0,0 +1,106 @@
+%% Problem 1
+
+% 3A-5C=  -41     5   -12
+%         -29    32   -39
+%         -12    22   -24
+
+% 7A+2B= Error using + , Matrix dimensions must agree.
+
+% CA=    -27    21   -38
+%        -54    33   -59
+%        -25    14   -25
+
+% CD'=   24    53     2   -23
+%        39  -133   -31   -41
+%        15   -96   -21   -24
+%% Problem 2
+
+% zeros(5)
+%      0     0     0     0     0
+%      0     0     0     0     0
+%      0     0     0     0     0
+%      0     0     0     0     0
+%      0     0     0     0     0
+% 
+% zeros(5,7)
+%      0     0     0     0     0     0     0
+%      0     0     0     0     0     0     0
+%      0     0     0     0     0     0     0
+%      0     0     0     0     0     0     0
+%      0     0     0     0     0     0     0
+% 
+% ones(5)  
+%      1     1     1     1     1
+%      1     1     1     1     1
+%      1     1     1     1     1
+%      1     1     1     1     1
+%      1     1     1     1     1
+% 
+% ones(5,7)
+%      1     1     1     1     1     1     1
+%      1     1     1     1     1     1     1
+%      1     1     1     1     1     1     1
+%      1     1     1     1     1     1     1
+%      1     1     1     1     1     1     1
+% 
+% size(D) ans =
+% 
+%      4     3
+%      
+% zeros(size(D))
+% 
+% ans =
+% 
+%      0     0     0
+%      0     0     0
+%      0     0     0
+%      0     0     0
+%      
+% diag([1 2 3 4])
+% 
+% ans =
+% 
+%      1     0     0     0
+%      0     2     0     0
+%      0     0     3     0
+%      0     0     0     4
+%      
+% eye(7)
+% 
+% ans =
+% 
+%      1     0     0     0     0     0     0
+%      0     1     0     0     0     0     0
+%      0     0     1     0     0     0     0
+%      0     0     0     1     0     0     0
+%      0     0     0     0     1     0     0
+%      0     0     0     0     0     1     0
+%      0     0     0     0     0     0     1
+%% Problem 3
+% 
+% [A,B]
+% Error using horzcat
+% Dimensions of matrices being concatenated are not consistent.
+% 
+% [A;B]
+% Error using vertcat
+% Dimensions of matrices being concatenated are not consistent.
+%% Problem 4
+% list=[diag([5 5 5 5 5 5 5]),[5;5;5;5;5;5;5]]
+% list =
+%      5     0     0     0     0     0     0     5
+%      0     5     0     0     0     0     0     5
+%      0     0     5     0     0     0     0     5
+%      0     0     0     5     0     0     0     5
+%      0     0     0     0     5     0     0     5
+%      0     0     0     0     0     5     0     5
+%      0     0     0     0     0     0     5     5
+%% Problem 5
+% A(i,:) 
+%Subscript indices must either be real %positive integers or logicals.
+%if (i) integer numbr %A(i,:)
+%it well show us all of items of row n. 
+%A(:,j)
+%Subscript indices must either be real %positive integers or logicals.
+%if (j) integer numbr %A(:,j)it well show us all of items of clumn j.
+%% By Muhammad Osama Abdulgelil Abdilsatar 3B
